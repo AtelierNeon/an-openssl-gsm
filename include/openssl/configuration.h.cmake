bf8da12e6d105505acebaae2afd09f910868fa39
Kene Lin <kenelin@gmail.com>
2026-06-22 10:49:19 +0800

    Sync openssl from https://github.com/openssl/openssl/tree/openssl-3.5.7, part 2

--- include/openssl/configuration.h.cmake
+++ include/openssl/configuration.h.cmake
@@ -64,9 +64,6 @@ extern "C" {
 # ifndef OPENSSL_NO_DEVCRYPTOENG
 #  define OPENSSL_NO_DEVCRYPTOENG
 # endif
-# ifndef OPENSSL_NO_DOCS
-#  define OPENSSL_NO_DOCS
-# endif
 # ifndef OPENSSL_NO_EC_NISTP_64_GCC_128
 #  define OPENSSL_NO_EC_NISTP_64_GCC_128
 # endif
